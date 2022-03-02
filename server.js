0143a779f63e273cd81b0281774a50afc4e2e245
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-02 14:04:28 -0500

    Magic 8 Ball express application complete

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 require('dotenv').config()
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-    console.log("Listening on port", port);
+    console.log('Listening on port', port);
 });
 
 
@@ -19,4 +19,13 @@ app.get('/greeting/:name', (req, res) => {
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     const tipAmount = req.params.tipPercentage * req.params.total / 100;
 	res.send(`${tipAmount}`);
+});
+
+
+// ************* Magic 8 Ball ************* //
+app.get('/magic/:question', (req, res) => {
+    const responseArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+    const adjustedQuestion = req.params.question.split("%20").join(" ");
+    const responseChoice = Math.floor(Math.random() * responseArray.length);
+    res.send(adjustedQuestion + '? <br>' + responseArray[responseChoice]);
 });
